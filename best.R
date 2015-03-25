31ac5296b6771b388422e37a3e351e90c8833231
unknown <JSS@JSS-E6330.harrisassoc.int>
2015-03-25 07:04:17 -0500

    Initial commit

---
+++ best.R
@@ -0,0 +1,11 @@
+# best.R
+#
+# Function takes two arguments: 
+# the 2-character abbreviated name of a state and anvoutcome name. 
+#
+# The function reads the outcome-of-care-measures.csv file and 
+# returns a character vector with the name of the hospital that 
+# has the best (i.e. lowest) 30-day mortality for the specified 
+# outcome in that state
+
+function <- best()
